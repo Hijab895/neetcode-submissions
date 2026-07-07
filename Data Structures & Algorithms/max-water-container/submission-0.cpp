d124d32ce732ce34dfc314b9d4820c2d08580a8f
Hijab Fatima <hijabf7239@gmail.com>
2026-07-07 20:25:25 +0500

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+//brute force
+    int maxArea(vector<int>& heights) {
+        int res = 0;
+        for (int i = 0; i < heights.size(); i++) {
+            for (int j = i + 1; j < heights.size(); j++) {
+                res = max(res, min(heights[i], heights[j]) * (j - i));
+            }
+        }
+        return res;
+    }
+};
